390da7c49ef9c30b848c8a05893825920a92a73a
maga <romalutfullaev@gmail.com>
2024-03-12 06:59:50 +0500

    add: Clear method

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,19 +1,26 @@
 import {defineStore} from "pinia";
 import {ref} from "vue";
 
+const initial = {
+  product: {},
+  ff: {},
+  bills: {},
+  marketplace: {},
+  batchCalc: {}
+}
 export const useStore = defineStore('store', () => {
-  const state = ref(JSON.parse(localStorage.getItem('store')) || {
-    product: {},
-    ff: {},
-    bills: {},
-    marketplace: {},
-    batchCalc: {}
-  })
+  const state = ref(JSON.parse(localStorage.getItem('store')) || initial)
 
   const changeState = (val) => {
     state.value = {...state.value, val}
     localStorage.setItem('store', JSON.stringify(state.value))
   }
 
-  return {state, changeState}
+  const clear = () => {
+    state.value = initial
+    localStorage.setItem('store', JSON.stringify(initial))
+    location.reload()
+  }
+
+  return {state, changeState, clear}
 })
---
+++ src/ui/HintUI.vue
@@ -0,0 +1,66 @@
+<script setup>
+defineProps({ hint: String })
+</script>
+
+<template>
+  <div class="hint">
+    <span>?</span>
+    <div>{{ hint }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hint {
+  position: relative;
+  display: inline-block;
+  z-index: 2;
+
+  &:hover div {
+    display: block;
+  }
+  span {
+    width: 16px;
+    border-radius: 50%;
+    height: 16px;
+    background: rgba(var(--primary-rgb), 16%);
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: .3s;
+    font-size: 12px;
+    user-select: none;
+
+    &:hover {
+      background: var(--primary);
+    }
+  }
+
+  div {
+    display: none;
+    position: absolute;
+    left: 50%;
+    transform: translate3d(-30px, 10px, 0);
+    background-color: var(--primary);
+    color: #fff;
+    padding: 8px;
+    border-radius: 4px;
+    font-size: 12px;
+    width: max-content;
+    max-width: 150px;
+
+    &::before {
+      content: '';
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 0 7.5px 12px 7.5px;
+      border-color: transparent transparent var(--primary) transparent;
+      transform: rotate(0deg);
+      position: absolute;
+      top: -5px;
+      left: 22px;
+    }
+  }
+}
+</style>
